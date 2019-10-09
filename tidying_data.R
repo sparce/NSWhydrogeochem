87d4c0e3a38e54510554f1ed540f22b5e552df95
Plet <ple017@csiro.au>
2019-10-09 21:04:49 +0800

    create variables with csv table from data

---
+++ tidying_data.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+S_Cobar<-read_csv("data/NSWSouthCobar_Entity-WaterSample.csv")
+Geochem1<- read_csv("data/NSWGeochemistry1_Entity-WaterSample.csv")
+Sample_repAug2019<- read_csv("data/CSIRO sample report Aug 2019cp.csv")
+Alks_Aug2019<-read_csv("data/GS New South Wales and Mt. Isa ALKS 05082019cp.csv")
+Asu<-read_csv("data/asu19100cp.csv")
