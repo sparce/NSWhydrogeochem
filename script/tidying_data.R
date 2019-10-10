349af9c3ecf16edc2f1a30908cb2fa4110708f6d
Plet <ple017@csiro.au>
2019-10-10 09:37:22 +0800

    initial tidying data select column+unite+toupper

--- script/tidying_data.R
+++ script/tidying_data.R
@@ -1,2 +1,20 @@
 library(tidyverse)
 
+S_Cobar<-read_csv("data/NSWSouthCobar_Entity-WaterSample.csv")
+Geochem1<- read_csv("data/NSWGeochemistry1_Entity-WaterSample.csv")
+Sample_repAug2019<- read_csv("data/CSIRO sample report Aug 2019cp.csv")
+Alks_Aug2019<-read_csv("data/GS New South Wales and Mt. Isa ALKS 05082019cp.csv")
+Asu<-read_csv("data/asu19100cp.csv")
+
+
+#work on datasheet Entity-watersample
+
+
+Minex_S_Cobar<-Geochem1 %>%
+  select(7:9,12,14,17,20:26) %>% #extract columns of interest
+  unite(1,2,3,4, col= "SampleID", sep="-")#combine column to get sampleID
+  mutate("SampleName"=toupper(SiteName))#need to homogenise samplename to capital
+  #need to split row into 2 when duplicate sampling
+  #Watertable homogenise to number and split into other column for accuracy that is defined by either a space or not separated only followed by ?
+  
+  
