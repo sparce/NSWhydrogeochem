68a4ac1bf927511b0785450e05ac8a26049ce5e4
Plet <ple017@csiro.au>
2019-10-17 12:20:57 +0800

    read repAugust 2019 with skip to get column names

--- script/tidying_data.R
+++ script/tidying_data.R
@@ -2,8 +2,11 @@ library(tidyverse)
 
 S_Cobar<-read_csv("data/NSWSouthCobar_Entity-WaterSample.csv")
 Geochem1<- read_csv("data/NSWGeochemistry1_Entity-WaterSample.csv")
-Sample_repAug2019<- read_csv("data/CSIRO sample report Aug 2019cp.csv")
+
+Sample_repAug2019<- read_csv("data/CSIRO sample report Aug 2019cp.csv", skip=11)
+  
 Alks_Aug2019<-read_csv("data/GS New South Wales and Mt. Isa ALKS 05082019cp.csv")
+
 Asu<-read_csv("data/asu19100cp.csv", skip = 11)
 fixnamesAsu<-Asu 
   fixnamesAsu<- paste(colnames(fixnamesAsu),fixnamesAsu[1,])
@@ -61,8 +64,12 @@ filter( str_starts(SampleName, pattern = "STD"))
  
  asu_tidy<-Asu %>% 
    select(2,3,5:12,14:18,20:37,39:50) %>% 
-   rename( SampleID_AN = X2, SampleID_CAT = X3)
-
+   rename( SampleID_AN = 'X2 Sample ID', SampleID_CAT = 'X3 NA') 
+   
+ asu_tidy_zero<- asu_tidy %>% 
+   mutate_all(~if_else(. == str_detect(.,"<0"),"0",.))
+              #mutate_all(~if_else(. == "<0.05", "0", .))
+ 
  
 
  
